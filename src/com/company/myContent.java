9ee9035f5cc3e5cab25d64e47cc3756ac32679b7
bulat15g <bulat15g@gmail.com>
2017-01-08 18:26:10 +0300

    hi

--- src/com/company/myContent.java
+++ src/com/company/myContent.java
@@ -10,56 +10,98 @@ import java.io.File;
 import java.io.IOException;
 
 
-
-public class myContent extends JPanel{
-    public Timer timerDraw,timerUpdate;
+public class myContent extends JPanel {
+    public Timer timerDraw, timerMoving;
     private BufferedImage image1;
-    public static Integer persX=0,persY=0;
+    public static Integer persX = 0, persY = 0;
 
-    public myContent(){
+    public final static int UP=1;
+    public final static int RIGHT=2;
+    public final static int DOWN=3;
+    public final static int LEFT=4;
+
+    public myContent() {
 
         initiate();
 
-        timerDraw=new Timer(1, new ActionListener(){
-            public void actionPerformed(ActionEvent e ) {
+        timerDraw = new Timer(1, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 repaint();
             }
         });
         timerDraw.start();
 
-        timerUpdate = new Timer(800, new ActionListener(){
-            public void actionPerformed(ActionEvent e ) {
 
-                move(20,20);
+    }
+
+    private final int dx = 5;
+    private final int dy = 5;
+    int kx = 1;
+    int ky = 1;
+
+
+    /**
+     * @param direction 1- up 2- right 3 - down 4 - left
+     */
+    public void startMoving(int direction) {
+
+        if(timerMoving!=null&&timerMoving.isRunning()){
+            timerMoving.stop();
+        }
+
+
+
+
+        switch (direction) {
+            case 1: {
+                kx = 0;
+                ky = -1;
+                break;
+            }
+            case 2: {
+                kx = 1;
+                ky = 0;
+                break;
+            }
+            case 3: {
+                kx = 0;
+                ky = 1;
+                break;
+            }
+            case 4: {
+                kx = -1;
+                ky = 0;
+                break;
+            }
+        }
+
+        timerMoving = new Timer(200, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                persX += kx * dx;
+                persY += ky * dy;
 
             }
         });
-        timerUpdate.start();
-    }
-
-    public void move(int x,int y){
-        persX+=x;
-        persY+=y;
-    }
-
-
-
-
-    public void paint(Graphics g){
-
-        g.clearRect(0,0,getWidth(),getHeight());
-        g.fillOval(100,100,50,40);
-
-
-        g.drawImage(image1,300+persX,300+persY,null);
-
-
-
+        timerMoving.setRepeats(false);
+        timerMoving.start();
 
 
     }
 
-    public void initiate(){
+
+    public void paint(Graphics g) {
+
+        g.clearRect(0, 0, getWidth(), getHeight());
+        g.fillOval(100, 100, 50, 40);
+
+
+        g.drawImage(image1, 300 + persX, 300 + persY, null);
+
+
+    }
+
+    public void initiate() {
         try {
             image1 = ImageIO.read(new File("pers.png"));
         } catch (IOException ex) {
